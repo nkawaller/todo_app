08ebad21037f23246dfb92009ddf51e6ddb682cb
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-14 11:01:08 -0500

    Added JS pipeline

---
+++ static/index-bundle.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!*************************!*\
+  !*** ./assets/index.js ***!
+  \*************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("function component() {\n    const element = document.createElement('div');\n    element.innerHTML = 'Hello webpack';\n    return element;\n  }\n  document.body.appendChild(component());\n\n//# sourceURL=webpack://todo_app/./assets/index.js?");
+/******/ })()
+;
