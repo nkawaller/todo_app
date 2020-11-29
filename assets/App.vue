ee59462b1dc7d4ff388fa1332c2415ec1083fc44
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-29 10:19:51 -0500

    wrapped ul in base card

--- assets/App.vue
+++ assets/App.vue
@@ -1,13 +1,18 @@
 <template>
+<div>
   <the-header title='greeting'></the-header>
+  <note-list></note-list>
+    </div>
 </template>
 
 <script>
 import TheHeader from './components/layouts/TheHeader.vue';
+import NoteList from './components/todo_list/NoteList.vue';
 
 export default {
   components: {
-    TheHeader
+    TheHeader,
+    NoteList
   },
   data() {
     return {
---
+++ assets/components/todo_list/NoteList.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <base-card class="base-card">
+    <h1>UL will go here</h1>
+    <ul>
+        <li>Todo 1</li>
+        <li>Todo 2</li>
+        <li>Todo 3</li>
+    </ul>
+    </base-card>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
