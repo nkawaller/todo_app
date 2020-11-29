bdab9f041135a6a998482c40f01e516178c72832
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-29 10:02:15 -0500

    created base card

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import BaseCard from './components/UI/BaseCard.vue';
+
+Vue.component('base-card', BaseCard);
 
 new Vue({
    el: "#app",
